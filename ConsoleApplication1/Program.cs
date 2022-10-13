17821bdb410be411035dafba4faffca6bbe4870e
Yunus Furkan Özer <bardakta.kahve@gmail.com>
2022-10-13 22:23:51 +0300

    sj

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("bir sayı girin");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            int i, j;
+            for (i = 1; i <= sayi; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("" + (j));
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
